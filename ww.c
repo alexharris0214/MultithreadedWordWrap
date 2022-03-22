ce436386735666b59fb5f2f0125a280b39f90142
UAmaxim <yacunmaxim@gmail.com>
2022-03-22 13:49:41 -0400

    Logic for writing to output

--- ww.c
+++ ww.c
@@ -7,6 +7,8 @@
 #include <fcntl.h>
 #include <string.h>
 
+#define BUFFER_SIZE 16
+
 
 /*
 * Returns a string with the prefix "wrap."
@@ -15,7 +17,7 @@
 * lying within the specified directory
 */
 char *getOutputName(char *output){
-    char *outputFileName = malloc(sizeof(output) + 6); // 6 is hardcoded to be length of "wrao." (5 character), and 1 for string terminator
+    char *outputFileName = (char *)malloc(sizeof(output) + 5); // 5 is hardcoded to be length of "wrap." (5 character)
 
     // manually assining the prefix
     outputFileName [0]= 'w';
@@ -28,31 +30,64 @@ char *getOutputName(char *output){
     for(int i = 0; i<sizeof(output); i++){
         outputFileName[5+i] = output[i];
     }
-    // placing terminator character
-    outputFileName[sizeof(output) + 5] = '\0';
 
     return outputFileName;
 }
 
-void reformatFile(int fd, int lineLength, char *output){
+int reformatFile(int fd, int lineLength, char *output){
     int outputFile = 1; // by default, output is to STDOUT
-    char *buff = malloc(lineLength);
 
     if(output != NULL){ // if we have a desired outputFile, switch to that
-        char *outputFileName = getOutputName(output);
-        outputFile = open(outputFileName, O_WRONLY|O_CREAT|O_TRUNC, 0700); // creating file if it does not exist with user RWX permissions
-        if(outputFile <= 0){ // checking to see if open returned successfully
-            puts("Could not open/create desired outpfile");
+        outputFile = open(getOutputName(output), O_WRONLY|O_CREAT|O_TRUNC, 0700); // creating file if it does not exist with user RWX permissions
+        if(outputFile <= 0){ // checking to see if open returned successfully   /* FIXME: DO WE WANT THIS TO BE <= 0 OR < 0?*/
+            puts("Could not open/create desired output file");
             perror(outputFile);
             return EXIT_FAILURE;
         }
     }
     
-    /* BEGING READ/WRITE IMPLEMENTATION HERE */
+    int lineSpot = 0;
+    int seenTerminator = 0;
+    int i;
 
+    char buffer[BUFFER_SIZE];
+    char *currWord = (char *)malloc(sizeof(char)*(lineLength + 1));      // should I do +1?
+    memset(currWord, '\0', sizeof(currWord));
 
-    // testing that writing is writing to correct place
-    write(outputFile, "Hlo", 3);
+    int bytesRead = read(fd, buffer, BUFFER_SIZE);
+
+    while(bytesRead > 0){
+        for(i = 0; i < bytesRead; i++){
+            if(isspace(buffer[i])){     //current character is whitespace
+                if(currWord[0] != '\0'){
+                    /*write currWord to outputFile*/
+                } else if(buffer[i] == '\n'){
+                    if(seenTerminator){
+                        /*write a line terminator to outputFile*/
+                        seenTerminator = 0;
+                    } else {
+                        seenTerminator = 1;
+                    }
+                }
+                memset(currWord, '\0', sizeof(currWord));  //clear currWord and start a new word
+
+            } else {                  //current character is non-whitespace
+                if(strlen(currWord) == (sizeof(currWord) - 1)){
+                    currWord = (char *)realloc(currWord, (sizeof(char)*(sizeof(currWord)*2)));
+                    memset(&currWord[strlen(currWord)], '\0', (sizeof(currWord) - strlen(currWord)));
+                }
+                strcat(currWord, &buffer[i]);
+            }
+        }
+        bytesRead = read(fd, buffer, BUFFER_SIZE);
+    }
+
+    if(sizeof(currWord) > (sizeof(char)*(lineLength + 1))){       //we had a word larger than a line
+        printf("ERROR: File %d contains a word longer than specified line length.", fd);                /* is there some way to get the file name to return instead of the descriptor? */
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
 
 
@@ -61,8 +96,9 @@ int main(int argc, char const *argv[])
     int fd = -1; // File pointer
     DIR *dr = -1; // Directory pointer
     struct stat statbuf; // Holds file information to determine its type
-    struct dirent *dp;
+    struct dirent *dp;                                                          /* What is dp */
     int err; // Variable to store error information
+    int exitFlag = EXIT_SUCCESS; //determine what exit status we return
     
     /*
     * Checks to see if valid arguments are passed in
@@ -84,17 +120,19 @@ int main(int argc, char const *argv[])
                 puts("Could not open directory");
             }
             chdir(argv[2]); // Changing the working directory to have access to the files we need
+            
             while ((dp = readdir(dr)) != NULL){ // while theyre are files to be read
 
                 // case where name of file is just a reference to current/parent directory
-                if(strcmp(dp->d_name, "..") ==0 || strcmp(dp->d_name, ".") ==0 || strstr(dp->d_name, "wrap") != NULL){
+                if(strcmp(dp->d_name, "..") ==0 || strcmp(dp->d_name, ".") ==0 || strstr(dp->d_name, "wrap") != NULL){    /* why both ".." and "."? */
                     continue;
                 }
         
                 // open the current file that we are on and work on it
                 fd = open(dp->d_name, O_RDONLY);
                 if(fd > 0){
-                    reformatFile(fd, argv[1], dp->d_name);
+                    if(reformatFile(fd, argv[1], dp->d_name) == EXIT_FAILURE)
+                        exitFlag = EXIT_FAILURE;
                 }
             }
         } else{ // file argument is a regular file
@@ -103,7 +141,7 @@ int main(int argc, char const *argv[])
                 puts("Could not open file");
                 return EXIT_FAILURE;
             }
-            reformatFile(fd, argv[1], NULL);
+            exitFlag = reformatFile(fd, argv[1], dp->d_name);
         }
         // no file was passed in, reading from standard input
     } else if(argc==2){
@@ -113,5 +151,5 @@ int main(int argc, char const *argv[])
         puts("Not enough arguments");
         return EXIT_FAILURE;
     }
-    return 0;
+    return exitFlag;
 }
